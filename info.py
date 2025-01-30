6ba4ddcdc63e6ee0a6fdb8e3d4375715d25ba9a7
Abhishek779-Star <abhishekbro779@gmail.com>
2025-01-30 11:07:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,10 +14,10 @@ def is_enabled(value, default):
         return default
 
 
-ADMIN = int(getenv("ADMIN", ""))
+ADMIN = int(getenv("ADMIN", "6570474744"))
 SILICON_PIC = os.environ.get("SILICON_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
-API_ID = int(getenv("API_ID", ""))
-API_HASH = str(getenv("API_HASH", ""))
+API_ID = int(getenv("API_ID", "16402669"))
+API_HASH = str(getenv("API_HASH", "e50b6c6e9dc8077ec3e9db0e565631e4"))
 BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
 FORCE_SUB = os.environ.get("FORCE_SUB", "") 
 MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://replacewithyourmongodb:replacewithyourmongodb@cluster0.zi78j51.mongodb.net/?retryWrites=true&w=majority",))
