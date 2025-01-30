68bf389862948fc4e912bdb7806965b5e0be29cc
mimamofficial1 <muzaffarimamneema@gmail.com>
2025-01-30 21:54:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,12 +17,9 @@ def is_enabled(value, default):
 ADMIN = int(getenv("ADMIN", "6139759254"))
 SILICON_PIC = os.environ.get("SILICON_PIC", "https://i.ibb.co/h9GBt42/file-6320.jpg")
 API_ID = int(getenv("API_ID", "23631217"))
-API_HASH = str(getenv("API_HASH", "
-e50b6c6e9dc8077ec3e9db0e565631e4
-
-"))
+API_HASH = str(getenv("API_HASH", "567c6df308dc6901790309499f729d12"))
 BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
-FORCE_SUB = os.environ.get("FORCE_SUB", "-1002006400723") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "-1002111679640") 
 MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://mrnoffice692:PsO4VGHI9heKd7WA@cluster0.o1vcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",))
 DEF_CAP = str(
     getenv(
